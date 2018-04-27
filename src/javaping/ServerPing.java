7a6ffd15ae47036b543f67727afc29e866767e00
Zekeriya İnce <mhdszekeriya@gmail.com>
2018-04-27 21:31:52 +0300

    Update ServerPing.java

--- src/javaping/ServerPing.java
+++ src/javaping/ServerPing.java
@@ -1,24 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package javaping;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.SocketException;
-import java.util.Random;
 
-/**
- *
- * @author Zekeriya
- */
+
+
 public class ServerPing {
 
     private static final double KAYIP_ORAN = 0.3; //Her paket cevap gondermemek icin kayıp
